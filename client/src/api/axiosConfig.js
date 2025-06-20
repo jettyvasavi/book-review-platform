753cf805ba1ef074a00517c935fc82f1b50e0f2f
unknown <jettyvasavi@gmail.com>
2025-06-20 23:56:32 +0530

    updated m

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // By setting baseURL to just '/api', all requests will be sent to the same domain
-  // that the frontend is hosted on.
-  // e.g., on my-site.vercel.app, it will call my-site.vercel.app/api/...
   baseURL: '/api',
 });
 
-// The rest of the file stays the same
 api.interceptors.request.use(
   (config) => {
     const userString = localStorage.getItem('user');
