1dbcb619acc53655d0a9694b969eeb6374a7246e
unknown <jettyvasavi@gmail.com>
2025-06-21 18:19:35 +0530

    updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,37 +1,32 @@
-import User from '../models/User.js'; 
-import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+// --- SWITCH BACK TO bcryptjs ---
+import bcrypt from 'bcryptjs';
+// -----------------------------
 import jwt from 'jsonwebtoken';
+
+// The rest of your registerUser and loginUser functions are
+// already correct and do not need to be changed.
+// The function names are identical between the two libraries.
 
 export const registerUser = async (req, res, next) => {
   const { name, email, password } = req.body;
-
   try {
     if (!name || !email || !password) {
       res.status(400);
       throw new Error('Please provide all required fields');
     }
-
     const userExists = await User.findOne({ email });
     if (userExists) {
       res.status(400);
       throw new Error('User with this email already exists');
     }
-
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    
-    const userData = {
+    const user = await User.create({
       name,
       email,
       password: hashedPassword,
-    };
-
-    console.log('ABOUT TO EXECUTE User.create() WITH DATA:', userData);
-   
-    const user = await User.create(userData);
-
-    console.log('User.create() has finished.');
-
+    });
     if (user) {
       res.status(201).json({
         _id: user._id,
@@ -40,10 +35,9 @@ export const registerUser = async (req, res, next) => {
         token: jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' }),
       });
     } else {
-      throw new Error('User creation returned null.');
+      throw new Error('Invalid user data');
     }
   } catch (error) {
-    console.error("ERROR in registerUser:", error);
     next(error);
   }
 };
