945a8d49eda9483f83227a3497f29d08ab777c71
unknown <jettyvasavi@gmail.com>
2025-06-21 17:34:33 +0530

    updated

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name:     { type: String, required: true }, 
-  email:    { type: String, required: true, unique: true },
+  name:     { type: String, required: true },
+  // --- ADD 'index: true' TO THE EMAIL FIELD ---
+  email:    { 
+    type: String, 
+    required: true, 
+    unique: true,
+    index: true // This tells MongoDB to optimize searches on this field
+  },
+  // ------------------------------------------
   password: { type: String, required: true },
   bio:      { type: String, default: '' }
 }, { timestamps: true });
