db355779b5c9f1692e64a01474d419f162b47ae8
unknown <jettyvasavi@gmail.com>
2025-06-21 12:31:46 +0530

    updated

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -1,10 +1,10 @@
 import Review from '../models/Review.js';
+import Book from '../models/Book.js'; 
 
 export const getReviewsByBook = async (req, res, next) => {
   try {
     const reviews = await Review.find({ bookId: req.params.bookId })
-      .populate('user', 'name'); 
-
+      .populate('user', 'name');
     res.json(reviews);
   } catch (error) {
     next(error);
@@ -20,8 +20,34 @@ export const addReview = async (req, res, next) => {
       res.status(400);
       throw new Error('Book ID, comment, and rating are required');
     }
-    const review = await Review.create({ bookId, comment, rating, user: userId });
-    res.status(201).json(review);
+
+    const book = await Book.findById(bookId);
+    if (!book) {
+      res.status(404);
+      throw new Error('Book not found');
+    }
+
+    const newReview = new Review({
+      bookId,
+      comment,
+      rating,
+      user: userId,
+    });
+    const savedReview = await newReview.save();
+
+    const reviews = await Review.find({ bookId });
+    
+    const totalRating = reviews.reduce((acc, item) => item.rating + acc, 0);
+    const averageRating = totalRating / reviews.length;
+
+    book.rating = Math.round(averageRating * 10) / 10;
+    
+    book.numReviews = reviews.length; 
+    
+    await book.save();
+
+    res.status(201).json(savedReview);
+
   } catch (error) {
     next(error);
   }
